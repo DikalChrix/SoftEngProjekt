672e995ec62a0813da8bf937c265207d6aac794e
VincentDahl <42547209+VincentDahl@users.noreply.github.com>
2019-05-05 11:16:15 +0200

    Mere til employee

--- SoftEngProjekt/src/MainCode/Employee.java
+++ SoftEngProjekt/src/MainCode/Employee.java
@@ -1,12 +1,49 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-class Employee {
+public class Employee {
 
-    private abstract String Name;
-    private abstract ArrayList<Employee> Employee;
+    private String Name;
+    private ArrayList<Employee> Employee = new ArrayList<Employee>();
+    private boolean invalidCommand = true;
+
+    public Employee(String Name) {
+        this.Name=Name;
+        ProjectList();
+        AssistanceRequest();
+
+        Scanner command = new Scanner(System.in);
+        String nextcommand = command.nextLine();
+
+        //make an if so that a project can b selected
+        while(true){
+
+            if(nextcommand.equalsIgnoreCase("add")) {
+                AddNonActivity();
 
 
-        public static void main (String[]args){
+            } else if(nextcommand.contains(get.projectlist())) {
+
+
+            } else {
+                System.out.println("Invalid command! Please enter a valid commnad to continue");
+            }
+        }
+    }
+
+    public static void ProjectList() {
+        // Use Employee id to get list of project, and display them along with the list
+        // of all projects
 
     }
+
+    public static void AddNonActivity() {
+        // Method to make the employee able to add an activity to all projects
+
     }
+
+    public static void AssistanceRequest() {
+        // Updates when employee logs in, and check is someone called for his help.
+
+    }
+}
